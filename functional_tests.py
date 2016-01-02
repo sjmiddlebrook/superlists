e4ab0655aaa1f2869fcfe1e996d77bcec0289ab4
Jack Middlebrook <jack.middlebrook@gmail.com>
2016-01-02 01:22:38 +0100

    refactor FT

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Marissa opens the homepage to check out to-do app
 		self.browser.get('http://localhost:8000')
@@ -36,7 +41,7 @@ class NewVisitorTest(unittest.TestCase):
 
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy plane tickets to Cartagena', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy plane tickets to Cartagena')
 
 		# There is still a text box inviting her to add another item. She
 		# enters "Find Airbnb in Cartagena"
@@ -47,8 +52,8 @@ class NewVisitorTest(unittest.TestCase):
 		# The page updates again and now shows both items on her list
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy plane tickets to Cartagena', [row.text for row in rows])
-		self.assertIn('2: Find Airbnb in Cartagena', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy plane tickets to Cartagena')
+		self.check_for_row_in_list_table('2: Find Airbnb in Cartagena')
 		
 		# Marissa wonders whether the site will remember her list. Then
 		# she sees that the site has generated a unique URL for her --
